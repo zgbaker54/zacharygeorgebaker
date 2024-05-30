60bf025335cbe2d1a1b5b7dabe81bfdf2c0b7c14
Zach Baker <zgbaker54@gmail.com>
2024-05-30 15:52:24 -0700

    update resume link and title prose

--- src/Landing.js
+++ src/Landing.js
@@ -32,7 +32,7 @@ export default function Landing(){
   </Fade>
 
   // Flavor text giving people an idea of what I'm looking for
-  let subtitle_text = `"Motivated and highly organized data-driven programmer from the country's top Graduate Biomedical Engineering program. I love to build tools and pipelines that automate and support data organization, visualization, and processing. Seeking a position that leverages my background, skills, and passion in an environment that fosters personal and professional growth."`
+  let subtitle_text = `"Motivated and highly organized data-driven developer experienced in the healthcare tech industry and educated at Hopkins. I love to build tools and pipelines that automate and support data organization, visualization, and processing. Seeking a position that leverages my background, skills, and passion in an environment that fosters personal and professional growth."`
   let subtitle = <Box>
     <Fade
       in={subtitle_fade0}
@@ -45,7 +45,7 @@ export default function Landing(){
 
   // Button linking to Google Doc of my general resume (view-only)
   let resume_button = <Button
-    href="https://drive.google.com/file/d/1NeXvNWkr6eeE24B6Zs7va1oA9I4PuS6g/view?usp=sharing"
+    href="https://drive.google.com/file/d/12UpXFV9i1btO7uiZuwosjuKZ8Bl-5G_p/view?usp=sharing"
     target="_blank"
     sx={selectionButtonSx}
   >
